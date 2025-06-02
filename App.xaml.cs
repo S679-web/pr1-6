dab610cf6d24fec80c9403f1988002fe58b501b8
S679-web <Zksergei297@gmail.com>
2025-06-02 10:29:33 +0500

    Add files via upload

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace pr1_6
+namespace pr11_13
 {
     /// <summary>
     /// Логика взаимодействия для App.xaml
--- ImageGalleryWindow_pr11.xaml.cs
+++ ImageGalleryWindow_pr11.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace pr11_13
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для ImageGalleryWindow_pr11.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ImageGalleryWindow_pr11 : Window
     {
-        public MainWindow()
+        public ImageGalleryWindow_pr11()
         {
             InitializeComponent();
         }
-        private void OpenImageGallery_Click(object sender, RoutedEventArgs e)
-        {
-            var galleryWindow = new ImageGalleryWindow();
-            galleryWindow.Show();
-        }
-    }sS
+    }
 }
